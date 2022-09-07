22ef46df7a2dc9ba3517c07d55c67b6a7ab332f4
Yousef Jafari <titan.2018.yj@gmail.com>
2022-09-07 14:08:28 -0700

    aria with v-0.3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AryiaWith</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="{{ asset('assets/css/scripts/style.css') }}">
 </head>
